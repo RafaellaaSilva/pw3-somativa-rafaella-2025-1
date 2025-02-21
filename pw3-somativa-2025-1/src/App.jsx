fa33d59557128fa9206a35538a9bdfee0443df7d
RafaellaaSilva <167482716+RafaellaaSilva@users.noreply.github.com>
2025-02-20 22:27:50 -0300

    Inicio da barra de navegação

--- pw3-somativa-2025-1/src/App.jsx
+++ pw3-somativa-2025-1/src/App.jsx
@@ -1,32 +1,12 @@
 import './App.css'
-import BookCard from './assets/components/BookCard'
-import CapaLivro from './assets/cavernas_aco.jpg'
+import NavBar from './assets/components/layout/NaveBar'
 
 function App() {
 
   return (
     <>
       <div>
-        <h1>PW3 - WEBAPP - LIVRARIA</h1>
-        <BookCard 
-          titulo='As Cavernas de Aço'
-          autor='Isaac Azimov'
-          imagem={CapaLivro}/>
-
-        {/* <BookCard 
-          titulo='O Sol Desvelado'
-          autor='Isaac Azimov'
-          imagem='Uma imagem vai aparecer aqui'/>
-
-        <BookCard 
-          titulo='O Fim da Infaância'
-          autor='Arthur C. Clark'
-          imagem='Uma imagem vai aparecer aqui'/>
-
-        <BookCard 
-          titulo='Neuromancer'
-          autor='Willian Gibson'
-          imagem='Uma imagem vai aparecer aqui'/> */}
+        <NavBar />
       </div>
     </>
   )
