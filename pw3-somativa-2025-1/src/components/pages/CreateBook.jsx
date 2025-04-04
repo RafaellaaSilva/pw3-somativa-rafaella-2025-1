0fb5e27bd6d5f8079e8428ce095db2d7562cb119
RafaellaaSilva <167482716+RafaellaaSilva@users.noreply.github.com>
2025-04-03 22:09:07 -0300

    Finalizando conexão com o Back, Front e Banco

--- pw3-somativa-2025-1/src/components/pages/CreateBook.jsx
+++ pw3-somativa-2025-1/src/components/pages/CreateBook.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import style from './CreateBook.module.css'
 
 import Input from "../form/Input";
@@ -8,8 +8,9 @@ import Button from "../form/Button";
 
 const CreateBook = ()=>{
 
-    // CRIA A ESTRUTURA DE STATE PARA OS DADOS DE LIVRO
     const [book, setBook] = useState({});
+
+    const [categories, setCategories] = useState([]);
 
     // CAPTURA DE DADOS DO ELEMENTO DE INPUT
     function handlerChangeBook(event){
@@ -19,13 +20,55 @@ const CreateBook = ()=>{
 
     // CAPTURA DE DADOS DO ELEMENTO DE SELECT
     function handlerChangeCategory(event){
-        setBook({... book, category : event.target.options[event.target.selectedIndex].text});
+        setBook({... book, cod_categoria : event.target.options[event.target.selectedIndex].value});
     }
 
     // ENVIO DOS DADOS PARA A API
     function submit(event){
         event.preventDefault();
         console.log(book);
+        insertBook(book);
+    }
+
+    // RECUPERA OS DADOS DE CATEGORIA DA APIREST:
+    useEffect(() => {
+        fetch('http://127.0.0.1:5000/listagemCateorias', {
+            method: 'GET',
+            headers: {
+                'Content-Type':'application/json',
+                'Access-Control-Allow-Origin':'*',
+                'Access-Control-Allow-Headers':'*'
+            }
+
+        }).then((resp) =>
+            resp.json()
+        ).then((categorias) => {
+            console.log('TESTE: ' + categorias.data);
+            setCategories(categorias.data);
+        }).catch((error) => {
+            console.log('ERRO: ' + error);
+        })
+    }, []);
+
+    //INSERÇÃO DE LIVRO
+    function insertBook(book) {
+            fetch('http://127.0.0.1:5000/inserirLivro', {
+                method: 'POST',
+                mode: 'cors',
+                headers: {
+                    'Content-Type':'application/json',
+                    'Access-Control-Allow-Origin':'*',
+                    'Access-Control-Allow-Headers':'*'
+                },
+                body : JSON.stringify(book)
+    
+            }).then((resp) =>
+                resp.json()
+            ).then((respJSON) => {
+                console.log('RESPOSTA ' + respJSON);
+            }).catch((error) => {
+                console.log('ERRO: ' + error);
+            })
     }
 
     return(
@@ -37,32 +80,33 @@ const CreateBook = ()=>{
                 <Input 
                 text='Nome do livro'
                 type='text'
-                name='txt_livro'
-                id='txt_livro'
+                name='nome_livro'
+                id='nome_livro'
                 placeholder='Digite o nome do livro'
                 handlerChange={handlerChangeBook} />
 
                 <Input 
                 text='Autor'
                 type='text'
-                name='txt_autor'
-                id='txt_autor'
+                name='autor_livro'
+                id='autor_livro'
                 placeholder='Digite o nome do autor'
                 handlerChange={handlerChangeBook} />
 
                 <Input 
                 text='Descrição'
                 type='text'
-                name='txt_descricao'
-                id='txt_descricao'
+                name='descricao_livro'
+                id='descricao_livro'
                 placeholder='Digite a descrição do livro'
                 handlerChange={handlerChangeBook} />
 
                 <Select 
-                name='slc_categoria'
-                id='slc_categoria'
+                name='cod_categoria'
+                id='cod_categoria'
                 text='Categoria do livro'
-                handlerChange={handlerChangeCategory} />
+                handlerChange={handlerChangeCategory}
+                options={categories} />
 
                 <Button 
                 label='CADASTRAR' />
