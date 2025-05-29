5c060c0bdc39d7634f6c9f0de2c598a84234109a
RafaellaaSilva <167482716+RafaellaaSilva@users.noreply.github.com>
2025-05-29 19:41:12 -0300

    Update and Delete

--- pw3-somativa-2025-1/src/App.jsx
+++ pw3-somativa-2025-1/src/App.jsx
@@ -9,6 +9,8 @@ import Home from './components/pages/Home'
 import CreateBook from './components/pages/CreateBook'
 import ListBook from './components/pages/ListBook'
 import DetailBook from './components/pages/DetailBook'
+import UpdateBooks from './components/pages/UpdateBooks'
+import DeleteBook from './components/pages/DeleteBook'
 
 // IMPORTAÇÃO DO NAVBAR
 import NavBar from './components/layout/NavBar'
@@ -30,7 +32,9 @@ function App() {
                 <Route path='/' element={<Home />} />
                 <Route path='/newBook' element={<CreateBook />} />
                 <Route path='/listBook' element={<ListBook />} />
-                <Route path='/detailBook/:cod_livro' element={<DetailBook/>} />
+                <Route path='/detailBook/:cod_livro' element={<DetailBook />} />
+                 <Route path='/deleteBook/:cod_livro' element={<DeleteBook />} />
+                <Route path='/updateBook/:cod_livro' element={<UpdateBooks />} />
 
               </Route>
 
